cb59ceb827848b0d776c0efb708a81c92bcb55b6
Vikram <vikramkini9@gmail.com>
2025-11-16 01:17:37 -0600

    pushing code to page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/raveena-portfolio/',
+  base: '/raveenakini-portfolio/',
   plugins: [react()],
 })
